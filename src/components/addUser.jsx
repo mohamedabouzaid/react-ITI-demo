b59822c5c25c6fb2edee79ef3d62ccac203ffe6e
abouzaid <mohmmdabouzaid@gmail.com>
2022-03-16 17:37:03 +0200

    add ref dom

--- src/components/addUser.jsx
+++ src/components/addUser.jsx
@@ -1,6 +1,9 @@
-import {useState,useEffect}from 'react';
+import {useState,useEffect,useRef}from 'react';
+
 
 const AddUser = (props) => {
+    //create ref
+    const inputEl=useRef(null);
    const {AddUser,isEnabled}=props
    const [user,setUser]=useState({name:'',age:''})
    const handleSubmit=(event)=>{
@@ -17,13 +20,21 @@ const AddUser = (props) => {
        if (! isEnabled) setUser({name:'',age:''})
    },[isEnabled])
 
+  //useeffect to do foucus in ref
+   useEffect(()=>{
+   
+   inputEl.current?.focus();
+
+
+  },[])
+
     return (
         <>
          <div>
             add user
         </div>
         <form onSubmit={handleSubmit}>
-           <input type='text' name='name' value={user.name} onChange={handleChange} />
+           <input type='text' name='name' value={user.name} onChange={handleChange} ref={inputEl}/>
            <input type='text' name='age'value={user.age}  onChange={handleChange} />
            <input type="submit" value='add' disabled={!isEnabled}/>
 
